7e78352b3d736a091cfba07a1ec4ea404178e4a2
qtu11 <trachduong93@gmail.com>
2025-06-04 20:56:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,1 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const port = 3000;
 
-// Phục vụ các file tĩnh từ thư mục hiện tại
-app.use(express.static(path.join(__dirname, '.')));
-
-// Route mặc định trả về index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-// Khởi động server
-app.listen(port, () => {
-  console.log(`Server đang chạy tại http://localhost:${port}`);
-});
