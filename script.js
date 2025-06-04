28c0a7abaaa1cfcbfa8fe0428fe210ebdf5ff4f7
qtu11 <trachduong93@gmail.com>
2025-06-04 18:01:38 +0700

    script.js

--- script.js
+++ script.js
@@ -5,11 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
    // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app" ;   
 import { getAnalytics } from "firebase/analytics" ;   
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = { 
   apiKey : "AIzaSyDGl114VDUnvUtVdl7vNd35lHEgXQIdaKs" , 
   authDomain : "tuquangmemmery.firebaseapp.com" , 
@@ -20,9 +16,7 @@ const firebaseConfig = {
   measurementId : "G-W04JHPDT9Z" 
 };
 
-// Initialize Firebase
-const app = initializeApp ( firebaseConfig );
-const analytics = getAnalytics ( app );
+
     firebase.initializeApp(firebaseConfig);
     database = firebase.database();
     console.log("Firebase khởi tạo thành công"); // Debug
