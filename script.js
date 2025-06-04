54c761c169cc70e9b741d5ba9e40630fd37bc0c8
qtu11 <trachduong93@gmail.com>
2025-06-04 18:00:34 +0700

    script.js

--- script.js
+++ script.js
@@ -2,15 +2,27 @@ document.addEventListener("DOMContentLoaded", () => {
   // Khởi tạo Firebase
   let database;
   try {
-    const firebaseConfig = {
-      apiKey: "AIzaSyDGl114VDUnvUtVdl7vNd35lHEgXQIdaKs",
-      authDomain: "tuquangmemmery.firebaseapp.com",
-      projectId: "tuquangmemmery",
-      storageBucket: "tuquangmemmery.firebasestorage.app",
-      messagingSenderId: "280703846268",
-      appId: "1:280703846268:web:d4fc49d0750dcea82a46f9",
-      measurementId: "G-W04JHPDT9Z"
-    };
+   // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app" ;   
+import { getAnalytics } from "firebase/analytics" ;   
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = { 
+  apiKey : "AIzaSyDGl114VDUnvUtVdl7vNd35lHEgXQIdaKs" , 
+  authDomain : "tuquangmemmery.firebaseapp.com" , 
+  projectId : "tuquangmemmery" , 
+  storageBucket : "tuquangmemmery.firebasestorage.app" , 
+  messagingSenderId : "280703846268" , 
+  appId : "1:280703846268:web:d4fc49d0750dcea82a46f9" , 
+  measurementId : "G-W04JHPDT9Z" 
+};
+
+// Initialize Firebase
+const app = initializeApp ( firebaseConfig );
+const analytics = getAnalytics ( app );
     firebase.initializeApp(firebaseConfig);
     database = firebase.database();
     console.log("Firebase khởi tạo thành công"); // Debug
