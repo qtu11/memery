b9dd23dd6d5f88e5093ab3303fcbe5d1d1a0df6b
qtu11 <trachduong93@gmail.com>
2025-06-05 17:47:16 +0700

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,16 +1,1 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
 
-let messages = [];
-
-app.get('/api/messages', (req, res) => {
-  res.json(messages);
-});
-
-app.post('/api/messages', (req, res) => {
-  messages.push(req.body);
-  res.sendStatus(200);
-});
-
-app.listen(3000, () => console.log('Server running on port 3000'));
